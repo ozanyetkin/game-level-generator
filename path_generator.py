52080136b38a65e803f4d65776a85884959bd86c
Ozan Yetkin <ozan.yetkin.579@gmail.com>
2024-05-30 21:08:53 +0300

    path generator seems like working, non-empty strings to be changed with letters and a single dot

--- path_generator.py
+++ path_generator.py
@@ -4,39 +4,69 @@ import numpy as np
 size_v = 7
 size_h = 11
 
-number_of_paths = 3
+number_of_paths = 2
 min_path_length = 4
 max_path_length = 8
 
 level = np.zeros((size_h, size_v))
 
-# Generate random continuous (can be non-linear) paths represented by 1
-# Line paths should have a minimum length of min_path_length and a maximum length of max_path_length
-# Paths should not be crossing each other
-for i in range(number_of_paths):
-    # Randomly select the starting point of the path
-    start = np.random.randint(0, size_h)
-    # Randomly select the ending point of the path
-    end = np.random.randint(0, size_h)
-    # Randomly select the path type (linear or non-linear)
-    path_type = np.random.randint(0, 2)
-    # Generate the path
-    if path_type == 0:
-        # Generate a linear path
-        if start < end:
-            level[start:end+1, i] = 1
+
+# Function to check if the position is valid (not touching borders or other paths)
+def is_valid_position(level, x, y):
+    if x <= 0 or x >= size_h - 1 or y <= 0 or y >= size_v - 1:
+        return False
+    if level[x, y] == 1:
+        return False
+    return True
+
+
+# Function to generate a random path
+def generate_path(level, path_length):
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]  # Right, Down, Left, Up
+    while True:
+        x, y = np.random.randint(1, size_h - 1), np.random.randint(1, size_v - 1)
+        if is_valid_position(level, x, y):
+            break
+    path = [(x, y)]
+    level[x, y] = 1
+
+    while len(path) < path_length:
+        np.random.shuffle(directions)
+        for dx, dy in directions:
+            nx, ny = path[-1][0] + dx, path[-1][1] + dy
+            if is_valid_position(level, nx, ny):
+                path.append((nx, ny))
+                level[nx, ny] = 1
+                break
         else:
-            level[end:start+1, i] = 1
-    else:
-        # Generate a non-linear path
-        level[start, i] = 1
-        level[start+1, i] = 1
-        level[end, i] = 1
-        level[end-1, i] = 1
+            # No valid move found, backtrack
+            if len(path) > 1:
+                path.pop()
+            else:
+                # Restart if we cannot make a path of the desired length
+                for px, py in path:
+                    level[px, py] = 0
+                return False
+    return True
 
 
-# Replace the path with 0 and 1 with ' ' and 'X' respectively
-level = np.where(level == 0, '..', 'XX')
+# Generate paths
+paths_generated = 0
+attempts = 0
+max_attempts = 1000 
+while paths_generated < number_of_paths and attempts < max_attempts:
+    path_length = np.random.randint(min_path_length, max_path_length + 1)
+    if generate_path(level, path_length):
+        paths_generated += 1
+    attempts += 1
 
-# Print the path with ' ' and 'X'
-print(level)
+# print(level)
+
+# Change the matrix into string represented by '..' and 'XX' for empty and path respectively
+level_str = ''
+for row in level:
+    for cell in row:
+        level_str += 'XX' if cell == 1 else '..'
+    level_str += '\n'
+
+print(level_str)
